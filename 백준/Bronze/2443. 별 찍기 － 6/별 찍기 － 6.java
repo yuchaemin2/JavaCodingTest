92bece30857c5d9939ad42bb5dba1ce9022c4b21
chaemin <101326708+yuchaemin2@users.noreply.github.com>
2024-12-02 08:19:50 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 284 ms, Memory: 19176 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i=0; i<n; i++){
+		    for(int j=0; j<i; j++){
+		        System.out.print(" ");
+		    }
+		    for(int k=0; k<n-i; k++){
+		        System.out.print("*");
+		    }
+		    for(int l=0; l<n-i-1; l++){
+		        System.out.print("*");
+		    }
+		    System.out.print("\n");
+		}
+	}
+}
