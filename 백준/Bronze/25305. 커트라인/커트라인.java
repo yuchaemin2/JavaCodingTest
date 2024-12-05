759f121e9388dceb9429d43cd1b3a402812151e5
chaemin <101326708+yuchaemin2@users.noreply.github.com>
2024-12-06 08:50:20 +0900

    [Bronze II] Title: 커트라인, Time: 224 ms, Memory: 19676 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i=0; i<n; i++){
+		    arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i=0; i<n; i++){
+		    if(i==n-k){
+		        System.out.println(arr[i]);
+		    }
+		}
+		
+	}
+}
